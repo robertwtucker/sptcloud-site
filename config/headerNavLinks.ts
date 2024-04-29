a6bec04a7cc1b5fb775f574af511d865521f03ea
Robert Tucker <robertwtucker@gmail.com>
2024-04-29 11:45:47 -0400

    refactor: site redesign

---
+++ config/headerNavLinks.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2024 Quadient Group AG
+ * SPDX-License-Identifier: MIT
+ */
+
+export default [
+  { href: '/', title: 'Demos' },
+  { href: '/', title: 'Resources' },
+  { href: '/', title: 'About' },
+]
